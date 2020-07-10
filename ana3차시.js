835077df8e192385c54b718ada2d6af654a8fdeb
손은성 <sses0623@gmail.com>
2020-07-10 14:51:06 +0900

    hello

---
+++ ana3차시.js
@@ -0,0 +1,41 @@
+// var name ="홍길동";
+// var job="student";
+// var a =`내 이름은 ${name}이고 직업은${job}입니다.`
+// console.log(a);
+// var hudi={
+//     name,job
+// }
+// console.log(hudi);  
+// const add1=(x,y)=>{
+//     return x+y;
+// }
+// const add1 = (x,y) => (x+y);
+
+// const add1 = (x,y) => x+y;
+
+// const not1 = x => !x;
+
+// console.log(add1(1,2));
+// console.log(not1(5));
+// var a=()=>{
+//     console.log("function 이다~~");
+// }
+// a();
+// var area =(width,height) => width*height;
+// console.log(area(2,4))
+// const relationship2 ={
+//     name:'zero',
+//     friends: ['nero','hero','xero'],
+//     logFriends(){
+//         this.friends.forEach()
+//     },
+// };
+// relationship2.logFriends();
+const AnA={sever,node,web}={
+    sever: "SEVER",
+    node:"NODE",
+    web:"WEB"
+};
+console.log(sever);
+console.log(node);
+console.log(web);
---
+++ newfile.js
@@ -0,0 +1,18 @@
+// var count=1;
+// for(var i=2;i<=10;i++){
+//     count=count+i;
+// }
+// console.log(count)
+// let a=1;
+// {
+//     let a=6;
+// }
+// console.log(a)
+// const a=10
+// console.log(a)
+let a, i;
+for (i=1;i<=5;i++){
+    for(a=0;a<i;a++){
+        console.log("*")
+    }
+}
